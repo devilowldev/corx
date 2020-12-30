10f43c89109025864f9fd3b7b9e46da80e80830a
1GPEX <1GPEX>
2020-12-29 21:01:12 -0800

    corex

--- src/modules/del.js
+++ src/modules/del.js
@@ -1,12 +1,9 @@
-const all = require('./all')
-
 module.exports = (db, params, table) => {
 
-
-        let fet = db.prepare(`SELECT * FROM ${table} WHERE ID = (?)`).get(params.id);
-        if(!fet) return false
-        else fet = JSON.parse(fet.json);
-        try { fet = JSON.parse(fet) } catch (e) {}
+    let fet = db.prepare(`SELECT * FROM ${table} WHERE ID = (?)`).get(params.id);
+    if(!fet) return false
+    else fet = JSON.parse(fet.json);
+    try { fet = JSON.parse(fet) } catch (e) {}
         db.prepare(`DELETE FROM ${table} WHERE ID = (?)`).run(params.id);
 
     return true;
